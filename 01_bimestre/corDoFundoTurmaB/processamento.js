d8a005bf7f5845e9136475949f8a3712101b1e44
Radames <rjhalmeman@gmail.com>
2025-03-27 15:34:33 -0300

    27/03/2025 - 15:34:33

---
+++ 01_bimestre/corDoFundoTurmaB/processamento.js
@@ -0,0 +1,6 @@
+document.getElementById("azul").addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
+document.getElementById("verde").addEventListener("click", function() {
+    document.body.style.backgroundColor = "green";
+});
