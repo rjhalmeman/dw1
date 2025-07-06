5e0b184e612865be9cb4b6a0ed6146e28c039a78
Radames <rjhalmeman@gmail.com>
2025-07-06 07:21:34 -0300

    Início do 2025-06-11-loginClientServer - 06/07/2025 - 07:21:34

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:3000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+const usuarios = [
+  { email: 'joao@email.com', senha: 'abc123', nome: 'João' },
+  { email: 'maria@email.com', senha: 'abcd', nome: 'Maria' }
+];
+
+const checkAuth = (req, res, next) => {
+  if (req.cookies.logado === 'true') {
+    next();
+  } else {
+    res.status(401).json({ authenticated: false });
+  }
+};
+
+app.post('/login', (req, res) => {
+  const { email, senha } = req.body;
+  const usuario = usuarios.find(u => u.email === email && u.senha === senha);
+  if (usuario) {
+    console.log("login bem sucedido para " + usuario.nome.toUpperCase());
+    res.cookie('logado', 'true', {
+      maxAge: 900000,
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true // isso deve ser mudado para false quando o programa for para produção
+    });
+    res.send({ success: true, nome: usuario.nome });
+  } else {
+    res.status(401).send({ success: false, message: 'Credenciais inválidas' });
+  }
+});
+
+app.get('/check-auth', checkAuth, (req, res) => {
+  res.send({ authenticated: true });
+});
+
+app.post('/logout', (req, res) => {
+  //precisa ser igual ao cookie criado
+  res.clearCookie('logado', {
+    sameSite: 'None',
+    secure: true,
+    httpOnly: true
+    // Não é necessário incluir maxAge ao limpar
+  });
+  console.log("Cookie 'logado' removido com sucesso");
+  res.send({ success: true });
+});
+
+app.listen(port, () => {
+  console.log(`✅ Servidor rodando em http://localhost:${port}`);
+});
