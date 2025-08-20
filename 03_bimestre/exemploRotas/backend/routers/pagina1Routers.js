d27e257486953a7030001092dfb4e6f5467c6bea
Radames <rjhalmeman@gmail.com>
2025-08-20 05:47:50 -0300

    20/08/2025 - 05:47:50

--- 03_bimestre/exemploRotas/backend/routers/pagina1Routers.js
+++ 03_bimestre/exemploRotas/backend/routers/pagina1Routers.js
@@ -5,6 +5,6 @@ const controller = require('../controllers/pagina1Controller');
 router.get('/', controller.mostrarPagina); //rota raiz para Página 1
 router.get('/vermelha', controller.paginaVermelha);
 // router.get('/azul', controller.paginaAzul); // só pode ter essa linha se o controller.paginaAzul estiver implementado
-// router.get('/verde', controller.paginaVerde);
+
 
 module.exports = router;
